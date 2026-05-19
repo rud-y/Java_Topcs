c3548b6da18d4abda34e18216e18a2cf43e17b23
rud-y <rudolf.zachar@gmail.com>
2026-05-19 12:41:35 +0100

    Stream operations(characters&numbers pool)

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
@@ -15,9 +14,46 @@ public class Main {
 
 
         Stream.concat(secondStream, firstStream)
-                .map(s -> s.charAt(0) + " - " + s)
-                .forEach(System.out::println);
+                .map(s -> s.charAt(0) + " - " + s);
+//                .forEach(System.out::println);
 
+
+        List<String> bingoPool = new ArrayList<>(75);
+        int start = 1;
+        for (char c : "BINGO".toCharArray()) {
+            for(int i = start; i < (start + 15); i++){
+                bingoPool.add("" + c + i);
+//                System.out.println(c  + " " + i);
+            }
+
+        start +=15;
+        }
+
+        Collections.shuffle(bingoPool);
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+        System.out.println("-----------------------");
+
+//        List<String> firstOnes = bingoPool.subList(0, 15);
+        List<String> firstOnes = new ArrayList<>(bingoPool.subList(0, 15));
+        firstOnes.sort(Comparator.naturalOrder());
+        firstOnes.replaceAll(s -> {
+            if (s.indexOf("G") == 0 || s.indexOf("O") == 0) {
+                String updated = s.charAt(0) + "-" + s.substring(1);
+                System.out.print(updated + " ");
+                return updated;
+            }
+            return s;
+        } );
+        System.out.println("\n--------------------------");
+
+        bingoPool.stream()
+                .limit(15)
+                .filter(s -> s.indexOf("G") == 0 || s.indexOf("O") == 0)
+                .map(s -> s.charAt(0) + "-" + s.substring(1))
+                .sorted()
+                .forEach(s -> System.out.print(s + " "));
 
 
     }
