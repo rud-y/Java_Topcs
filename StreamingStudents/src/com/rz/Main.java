a7f5b146781dec26e003dc8ec0997f4e17222e0c
rud-y <rudolf.zachar@gmail.com>
2026-08-05 12:36:11 +0100

    Student engagement system - Stream processing

---
+++ StreamingStudents/src/com/rz/Main.java
@@ -0,0 +1,9 @@
+package com.rz;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Course c1 = new Course("JS", "Welcome to JavaScript");
+    System.out.println("c1 course : " + c1.toString());
+  }
+}
