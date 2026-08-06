36957b2646e5c2d0a333a25fa4c6898645384d06
rud-y <rudolf.zachar@gmail.com>
2026-08-06 18:43:57 +0100

    Add Student, test in main

--- StreamingStudents/src/com/rz/Main.java
+++ StreamingStudents/src/com/rz/Main.java
@@ -1,9 +1,25 @@
 package com.rz;
 
+import java.util.stream.Stream;
+
 public class Main {
   public static void main(String[] args) {
 
     Course c1 = new Course("JS", "Welcome to JavaScript");
     System.out.println("c1 course : " + c1.toString());
+
+    Course pymc = new Course("PYMC", "Python Masterclass");
+    Course jmc = new Course("JMC", "jAVA Masterclass");
+//    Student john = new Student("AU", 2022, 40, "M", true, pymc, jmc);
+
+//    System.out.println("john: " + john);
+//
+//    john.watchLecture("JMC", 10, 5, 2024);
+//    john.watchLecture("PYMC", 9, 6, 2021);
+//    System.out.println(john);
+
+    Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+            .limit(10)
+            .forEach(System.out::println);
   }
 }
